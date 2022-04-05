b7ebc136207e83a8cdec687be4d3a5869e0ea873
Kharchenko Nikolay <ithimikat@gmail.com>
2022-04-05 17:54:44 +0300

    added photos

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -15,6 +15,7 @@ class SiteController extends AbstractController
         return $this->render('site/index.html.twig', []);
     }
 
+    //JSON ответ на AJAX запрос, возвращает все названия изображений в папке с фотографиями
     public function getPhotoNames(Request $request)
     {
         return new JsonResponse([
